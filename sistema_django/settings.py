68cf38eed213d665296bcb30296357dc3535267a
scorsato86murilo <scorsato86murilo@gmail.com>
2024-11-23 22:01:28 -0300

    configuracoes básicas

--- sistema_django/settings.py
+++ sistema_django/settings.py
@@ -27,6 +27,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'index'
 ]
 
 MIDDLEWARE = [
@@ -113,7 +114,7 @@ MEDIA_URL = '/media/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MESSAGE_TAGS = {
-    # constants.DEBUG: 'alert-primary',
+    constants.DEBUG: 'alert-primary',
     constants.ERROR: 'alert-danger',
     constants.SUCCESS: 'alert-success',
     constants.INFO: 'alert-info',
